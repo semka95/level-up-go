0e55c39aa9824715f0d6cabfa47bf6462c438662
semka95 <semen.zernov@live.ru>
2017-02-26 01:43:31 +0300

    Continue 3rd chapter

--- ch3/ch3.go
+++ ch3/ch3.go
@@ -6,9 +6,33 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Gopher")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from /articles/")
 	})
 
-	http.ListenAndServe(":3000", nil)
+	mux.HandleFunc("/articles/latest/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from /articles/latest/")
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Server", "Go Server")
+		fmt.Fprintf(w, `
+		<html>
+			<body>
+				Hello Gopher
+			</body>
+		</html>`)
+	})
+
+	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from /users")
+	})
+
+	mux.HandleFunc("/error/", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Error", 500)
+	})
+
+	http.ListenAndServe(":3000", mux)
 }
