5ffdef84cff11d8ac198ba596a51e878d017bbb4
semka95 <semen.zernov@live.ru>
2017-03-02 02:33:16 +0300

    Create gophr project, add bootstrap files and write simple fileserver

---
+++ gophr/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+	log.Fatal(http.ListenAndServe(":3000", mux))
+}
