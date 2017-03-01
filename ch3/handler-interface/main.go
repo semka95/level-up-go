e34ef38713f22a172e5152cf249586f00723b64a
semka95 <semen.zernov@live.ru>
2017-03-01 22:46:08 +0300

    Handler interfaces

--- ch3/handler-interface/main.go
+++ ch3/handler-interface/main.go
@@ -20,7 +20,24 @@ func (h UptimeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, fmt.Sprintf("Current uptime: %s", time.Since(h.Started)))
 }
 
+// SecretTokenHandler secures a request with a secret token
+type SecretTokenHandler struct {
+	next   http.Handler
+	secret string
+}
+
+func (h SecretTokenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("secret_token") == h.secret {
+		h.next.ServeHTTP(w, r)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	http.Handle("/", NewUptimeHandler())
+	http.Handle("/", SecretTokenHandler{
+		next:   NewUptimeHandler(),
+		secret: "Secret",
+	})
 	http.ListenAndServe(":3000", nil)
 }
